84c23b3d7840a9f26c64bf906b16e1b6f95823c3
eniek <apolinar@o2.pl>
2017-04-21 21:24:14 +0200

    unicjalizacja aplikacji

---
+++ shop/src/main/java/com/eccomerce/shop/ShopApplication.java
@@ -0,0 +1,17 @@
+package com.eccomerce.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by english on 4/19/17.
+ */
+
+@SpringBootApplication
+public class ShopApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+}
