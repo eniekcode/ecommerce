7e6b4b03e1ae0200862130eefbc69d7f41b82090
eniek <apolinar@o2.pl>
2017-04-24 23:18:27 +0200

    aplikacja dziala poprawnie

--- admin/src/main/java/com/ecommerce/admin/AdminApplication.java
+++ admin/src/main/java/com/ecommerce/admin/AdminApplication.java
@@ -2,12 +2,20 @@ package com.ecommerce.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import static com.ecommerce.admin.AdminApplication.BASE_PACAGE;
+
 /**
  * Created by english on 4/19/17.
  */
-
-@SpringBootApplication(scanBasePackages = {"com.eccomerce"})
+@EntityScan(basePackages= {BASE_PACAGE})
+@EnableJpaRepositories(basePackages= {BASE_PACAGE})
+@SpringBootApplication(scanBasePackages = {BASE_PACAGE})
 public class AdminApplication {
+
+    public static final String BASE_PACAGE = "com.ecommerce";
 
     public static void main(String[] args)
     {
